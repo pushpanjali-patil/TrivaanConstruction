27268c87be49ccd0ac7ca4cd0b31ce366bd50ef3
Esha Patil <pushpanjalipatil113@gmail.com>
2026-08-29 10:56:48 +0530

    fixes for navbar closed

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,5 +1,18 @@
-import { Component, HostListener, signal } from '@angular/core';
-import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  Component,
+  HostListener,
+  signal,
+  ElementRef,
+  Renderer2
+} from '@angular/core';
+
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive
+} from '@angular/router';
+
 import { filter } from 'rxjs';
 
 @Component({
@@ -13,20 +26,84 @@ import { filter } from 'rxjs';
   styleUrl: './navbar.scss'
 })
 export class Navbar {
-  constructor(private router: Router) {
-     this.router.events
-    .pipe(filter(event => event instanceof NavigationEnd))
-    .subscribe(() => {
-      this.isHomePage.set(this.router.url === '/');
-    });
 
-  this.isHomePage.set(this.router.url === '/');
-  }
   isScrolled = signal(false);
+
   isHomePage = signal(false);
 
-@HostListener('window:scroll')
-onScroll() {
-  this.isScrolled.set(window.scrollY > 50);
-}
+  constructor(
+    private router: Router,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {
+
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe(() => {
+
+        this.isHomePage.set(
+          this.router.url === '/'
+        );
+
+        this.closeMenu();
+
+      });
+
+    this.isHomePage.set(
+      this.router.url === '/'
+    );
+  }
+
+
+  @HostListener('window:scroll')
+  onScroll() {
+
+    this.isScrolled.set(
+      window.scrollY > 50
+    );
+
+  }
+
+
+  closeMenu(): void {
+
+    const navbarMenu =
+      this.elementRef.nativeElement
+        .querySelector('#navbarMenu');
+
+    const navbarToggler =
+      this.elementRef.nativeElement
+        .querySelector('.navbar-toggler');
+
+
+    if (!navbarMenu) {
+      return;
+    }
+
+
+    this.renderer.removeClass(
+      navbarMenu,
+      'show'
+    );
+
+
+    if (navbarToggler) {
+
+      this.renderer.setAttribute(
+        navbarToggler,
+        'aria-expanded',
+        'false'
+      );
+
+      this.renderer.addClass(
+        navbarToggler,
+        'collapsed'
+      );
+
+    }
+
+  }
+
 }
